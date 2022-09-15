eaa1da11be195dc5ee1b1f4ef214907a4301bf45
K_A_A <banderas91@mail.ru>
2022-09-15 10:22:47 +0300

    Задача-3

--- zadacha_3/Program.cs
+++ zadacha_3/Program.cs
@@ -1,40 +1,70 @@
-﻿/* Сформируйте двухмерный массив из неповторяющихся двузначных чисел (размер массива не более 50 элементов). Напишите программу, которая будет построчно выводить массив.
-Вариант создания функции:
-Проверить число на присутствие в массиве (bool Contains(int[,] arr, int value){...} ) */
-
-Console.Write("Введите количество строк: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите количество столбцов: ");
-int n = Convert.ToInt32(Console.ReadLine());
-if (m * n > 50)
+﻿/* Задача 3. Сформируйте двухмерный массив из неповторяющихся двузначных чисел  
+(размер массива не более 50 элементов). Напишите программу, которая будет построчно выводить массив. 
+Вариант создания функции: 
+Проверить число на присутствие в массиве (bool Contains(int[,] arr, int value){...} ) 
+Пример: 
+Массив размером 3 x 3 
+10 11 55 
+33 41 23 
+17 28 34 
+*/ 
+ 
+bool Contains(int[,] arr, int value) 
+{ 
+    bool result = true; 
+    for(int i = 0; i < arr.GetLength(0); i++) 
+    { 
+        for(int j = 0; j < arr.GetLength(1); j++) 
+        { 
+             
+            if(arr[i,j] == value) 
+            { 
+                result = false; 
+            } 
+        } 
+    } 
+    return result; 
+} 
+ 
+void Matrixt2D(int n, int m) 
+{ 
+    int[,] table = new int[n,m]; 
+    int value = 0; 
+    Console.WriteLine("Таблица случайных чисел: "); 
+    for(int i = 0; i < n; i++) 
+    { 
+        for(int j = 0; j < m; j++) 
+        { 
+            value = new Random().Next(10,100); 
+            if (i != 0 || j != 0) 
+            {     
+                if(Contains(table, value) == true) 
+                { 
+                    table[i,j] = value; 
+                    Console.Write($"{table[i,j]} "); 
+                } 
+                else 
+                { 
+                    j--; 
+                }   
+            } 
+            else 
+            { 
+                table[i,j] = new Random().Next(10,100); 
+                Console.Write($"{table[i,j]} "); 
+            } 
+        } 
+        Console.WriteLine(); 
+    } 
+} 
+ 
+Console.Write("Укажите кол-во строк N: "); 
+int n = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Укажите кол-во строк M: "); 
+int m = Convert.ToInt32(Console.ReadLine()); 
+ if (m * n > 50)
 {
     Console.WriteLine("Количество элементов в массиве не должно быть больше 50!");
     return;
 }
-
-int[,] array = new int[m, n];
-Console.WriteLine("Сформированный массив");
-
-
-static void fillArray(int[,] array)
-{
-    Random rand = new Random();
-
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = rand.Next(1, 100);
-            for (int k = 0; k < j; k++)
-                if (array[i, k] == array[i, j])
-                    j--;
-
-            Console.Write(array[i, j] + " ");
-        }
-        Console.WriteLine();
-    }
-}
-fillArray(array);
-
-
-
+Matrixt2D(n,m);
